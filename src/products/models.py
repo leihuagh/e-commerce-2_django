d055b7311cc748830726678ce79a19c388cfc418
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-30 21:24:40 +0200

    register product model into django admin

--- src/products/models.py
+++ src/products/models.py
@@ -6,3 +6,9 @@ class Product(models.Model):
   title = models.CharField(max_length=120)
   description = models.TextField()
   price = models.DecimalField(decimal_places=2, max_digits=20, default=19.99)
+
+  def __str__(self):
+    return self.title
+  
+  # def __unicode__(self): # in python 2
+  #   return self.title
