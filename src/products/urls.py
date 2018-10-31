6e7a35e8f1ef1b227afc81eae91ddfab6628ddd9
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-10-31 09:35:37 +0200

    detail cbv, fbv

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url
 
-from .views import ProductListView, product_list_view
+from .views import ProductListView, ProductDetailView, product_list_view, product_detail_view
 
 urlpatterns = [
   url(r'^$', ProductListView.as_view()),
-  url(r'^-fbv/$', product_list_view),
+  url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view()),
+
+
+
+  url(r'^fbv/$', product_list_view),
+  url(r'^(?P<pk>\d+)/fbv/$', product_detail_view),
 ]
 
--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
@@ -20,3 +20,25 @@ def product_list_view(request):
     'object_list': queryset
   }
   return render(request, 'products/list.html', context)
+
+
+class ProductDetailView(DetailView):
+  queryset = Product.objects.all()
+  template_name = "products/detail.html"
+
+  def get_context_data(self, *args, **kwargs):
+    context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+    print(context)
+    context['abc'] = "some other content"
+    print(context)
+    return context
+
+
+def product_detail_view(request, pk, *args, **kwargs):
+  # instance = Product.objects.get(pk=pk)
+  instance = get_object_or_404(Product, pk=pk)
+  context = {
+    'object': instance,
+    'abc': "some other content from function based view"
+  }
+  return render(request, 'products/detail.html', context)
