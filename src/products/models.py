5d673d4a1fb6a183190fddb8d6caf7a2a16a2b1c
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-02 21:07:00 +0200

    image field

--- src/products/models.py
+++ src/products/models.py
@@ -25,7 +25,7 @@ class Product(models.Model):
   title = models.CharField(max_length=120)
   description = models.TextField()
   price = models.DecimalField(decimal_places=2, max_digits=20, default=19.99)
-  image = models.FileField(upload_to=upload_image_path, null=True, blank=True)
+  image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
 
   def __str__(self):
     return self.title
