38b1e6c02299436801751d2b88c265f430a7f3d9
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-04 10:22:52 +0200

    model manager in detail cbv

--- src/products/views.py
+++ src/products/views.py
@@ -24,15 +24,21 @@ def product_list_view(request):
 
 
 class ProductDetailView(DetailView):
-  queryset = Product.objects.all()
+  # queryset = Product.objects.all()
   template_name = "products/detail.html"
 
   def get_context_data(self, *args, **kwargs):
     context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
-    print(context)
     context['abc'] = "some other content"
-    print(context)
     return context
+
+  def get_object(self, *args, **kwargs):
+    request = self.request
+    pk = self.kwargs.get('pk')
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
+      raise Http404("Product does't exist")
+    return instance
 
 
 def product_detail_view(request, pk, *args, **kwargs):
