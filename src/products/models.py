75853809eb6fc1b2357576a6429bb1b7816f93da
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-05 21:04:32 +0200

    active products

--- src/products/models.py
+++ src/products/models.py
@@ -11,8 +11,6 @@ def get_filename_ext(filepath):
 
 
 def upload_image_path(instance, filename):
-  print(instance)
-  print(filename)
   new_filename = random.randint(1, 1000000)
   name, ext = get_filename_ext(filename)
   final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
@@ -20,16 +18,21 @@ def upload_image_path(instance, filename):
 
 
 class ProductQuerySet(models.query.QuerySet):
-  def featured(self): # Product.objects.all()featured()
-    return self.filter(featured=True)
+  def active(self):
+    return self.filter(active=True)
+  def featured(self):
+    return self.filter(featured=True, active=True)
 
 
 class ProductManager(models.Manager):
   def get_queryset(self):
     return ProductQuerySet(self.model, using=self._db)
   
+  def all(self):
+    return self.get_queryset().active()
+  
   def featured(self): # Product.objects.featured()
-    return self.get_queryset().filter(featured=True)
+    return self.get_queryset().featured()
 
   def get_by_id(self, id):
     qs = self.get_queryset().filter(id=id) # self.get_queryset() == Product.objects
@@ -43,6 +46,7 @@ class Product(models.Model):
   price = models.DecimalField(decimal_places=2, max_digits=20, default=19.99)
   image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
   featured = models.BooleanField(default=False)
+  active = models.BooleanField(default=True)
 
 
   objects = ProductManager()
