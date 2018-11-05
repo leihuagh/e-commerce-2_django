b0ff3cee0529decd28ec9ecf75313fadc4d09431
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-05 22:15:12 +0200

    dynamic redirect to product detail page

--- src/products/urls.py
+++ src/products/urls.py
@@ -11,9 +11,9 @@ from .views import (
   )
 
 urlpatterns = [
-  url(r'^$', ProductListView.as_view()),
+  url(r'^$', ProductListView.as_view(), name='list'),
   # url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view()),
-  url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
+  url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(), name='detail'),
   # url(r'^featured/$', ProductFeaturedListView.as_view()),
   # url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
 
