a2bc26320516e6c2cebeffef20bdca59ed140378
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-09 20:31:43 +0200

    start tags app

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
-    'search.apps.SearchConfig'
+    'search.apps.SearchConfig',
+    'tags.apps.TagsConfig'
 ]
 
 MIDDLEWARE = [
