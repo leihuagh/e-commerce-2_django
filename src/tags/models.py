2b4e1ec56e33c2984ce3fe2a6081272eea7ba816
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-09 20:48:29 +0200

    tags many to many

--- src/tags/models.py
+++ src/tags/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.db.models.signals import pre_save, post_save
 
+from products.models import Product
 from products.utils import unique_slug_generator
 
 # Create your models here.
@@ -11,6 +12,7 @@ class Tag(models.Model):
   slug = models.SlugField()
   timestamp = models.DateTimeField(auto_now_add=True)
   active = models.BooleanField(default=True)
+  products = models.ManyToManyField(Product, blank=True)
 
   def __str__(self):
     return self.title
