dbae8e18bd887636caf2d8f2c5a636e4c1545da8
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-09 20:40:27 +0200

    tags model

--- src/tags/models.py
+++ src/tags/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
+from django.db.models.signals import pre_save, post_save
+
+from products.utils import unique_slug_generator
 
 # Create your models here.
+
+class Tag(models.Model):
+  title = models.CharField(max_length=120)
+  slug = models.SlugField()
+  timestamp = models.DateTimeField(auto_now_add=True)
+  active = models.BooleanField(default=True)
+
+  def __str__(self):
+    return self.title
+
+
+
+def tag_pre_save_receiver(sender, instance, *args, **kwargs):
+  if not instance.slug:
+    instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(tag_pre_save_receiver, sender=Tag)
