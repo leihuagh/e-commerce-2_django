7b2016d775ab8ece4674d2ea1cb67482d46a7242
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-10 19:29:34 +0200

    start making cart

--- src/carts/models.py
+++ src/carts/models.py
@@ -7,12 +7,23 @@ from products.models import Product
 
 User = settings.AUTH_USER_MODEL
 
+class CartManager(models.Manager):
+  def new(self, user=None):
+    user_obj = None
+    if user is not None:
+      if user.is_authenticated():
+        user_obj = user
+    return self.model.objects.create(user=user_obj)
+
+
 class Cart(models.Model):
   user = models.ForeignKey(User, null=True, blank=True)
   products = models.ManyToManyField(Product, blank=True)
   total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
   timestamp = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
+
+  objects = CartManager()
 
   def __str__(self):
     return str(self.id)
--- src/carts/views.py
+++ src/carts/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import render
 
+from .models import Cart
+
 # Create your views here.
 
+def cart_create(user=None):
+  cart_obj = Cart.objects.create(user=None)
+  print('Create new cart')
+  return cart_obj
+
+
+
 def cart_home(request):
-  # request.session.set_expiry(300) # 300 seconds == 5 minutes
-  # print(dir(request.session))
-  # key = request.session.session_key # key for logged in user
-  # request.session['cart_id'] = 12 # set
-  # request.session['username'] = request.user.username # set
-  # request.session['username'] # get
-  # request.session.get('username', 'UnKnown') # get
-
-
+  # if cart_id is None:
+  #   cart_obj = cart_create()
+  #   request.session['cart_id'] = cart_obj.id
+  # else:
   cart_id = request.session.get('cart_id', None)
-  if cart_id is None:# and isinstance(cart_id, int):
-    print('careate new cart')
-    request.session['cart_id'] = 12
+  qs = Cart.objects.filter(id=cart_id)
+  if qs.count() == 1:
+    cart_obj = qs.first()
+    if request.user.is_authenticated() and cart_obj.user is None:
+      cart_obj.user = request.user
+      cart_obj.save()
   else:
-    print('Cart ID exists')
+    cart_obj = Cart.objects.new(user=request.user)
+    request.session['cart_id'] = cart_obj.id
   return render(request, 'carts/home.html', {})
