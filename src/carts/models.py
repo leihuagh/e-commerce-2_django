0e9d9c2d1cff9f937c66361de697b66dc6d14e43
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-10 10:10:34 +0200

    cart model

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from django.conf import settings
+from products.models import Product
+
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
+
+class Cart(models.Model):
+  user = models.ForeignKey(User, null=True, blank=True)
+  products = models.ManyToManyField(Product, blank=True)
+  total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return str(self.id)
