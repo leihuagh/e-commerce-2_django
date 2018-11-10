68bca5cdf2d59eb06b6effe7955aaea65b2f3a52
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-10 10:20:01 +0200

    start creating cart

--- src/carts/views.py
+++ src/carts/views.py
@@ -5,12 +5,17 @@ from django.shortcuts import render
 def cart_home(request):
   # request.session.set_expiry(300) # 300 seconds == 5 minutes
   # print(dir(request.session))
+  # key = request.session.session_key # key for logged in user
+  # request.session['cart_id'] = 12 # set
+  # request.session['username'] = request.user.username # set
+  # request.session['username'] # get
+  # request.session.get('username', 'UnKnown') # get
 
 
-  key = request.session.session_key # key for logged in user
-  request.session['cart_id'] = 12 # set
-  request.session['username'] = request.user.username # set
-  request.session['username'] # get
-  request.session.get('username', 'UnKnown') # get
-
+  cart_id = request.session.get('cart_id', None)
+  if cart_id is None:# and isinstance(cart_id, int):
+    print('careate new cart')
+    request.session['cart_id'] = 12
+  else:
+    print('Cart ID exists')
   return render(request, 'carts/home.html', {})
