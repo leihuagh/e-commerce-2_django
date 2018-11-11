4acbadafd6454bec1a66d160acfb44a6e3f12cc9
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-11 12:35:41 +0200

    add, remove from cart in template

--- src/carts/views.py
+++ src/carts/views.py
@@ -41,12 +41,16 @@ def cart_home(request):
 
 
 def cart_update(request):
-  product_id = 1
-  product_obj = Product.objects.get(id=product_id)
-  cart_obj, new_obj = Cart.objects.new_or_get(request)
-  if product_obj in cart_obj.products.all():
-    cart_obj.products.remove(product_obj) # cart_obj.products.remove(product_id)
-  else:
-    cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+  product_id = request.POST.get('product_id')
+  if product_id is not None:
+    try:
+      product_obj = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+      return redirect('cart:home')  
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    if product_obj in cart_obj.products.all():
+      cart_obj.products.remove(product_obj) # cart_obj.products.remove(product_id)
+    else:
+      cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
   # return  redirect(product_obj.get_absolute_url())
   return redirect('cart:home')
