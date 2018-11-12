11d80f2f190f96f3d7fb25b889bf1d1f21a6b10b
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-12 13:11:37 +0200

    is safe url

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
 
 from .forms import LoginForm, RegisterForm
 # Create your views here.
@@ -26,14 +27,19 @@ def login_page(request):
   context = {
     "form": form
   }
-  
+  next_ = request.GET.get('next')
+  next_post = request.POST.get('next')
+  redirect_path = next_ or next_post or None
   if form.is_valid():
     username = form.cleaned_data.get("username")
     password = form.cleaned_data.get("password")
     user = authenticate(request, username=username, password=password)
     if user is not None:
       login(request, user)
-      return redirect("/")
+      if is_safe_url(redirect_path, request.get_host()):
+        return redirect(redirect_path)
+      else:
+        return redirect("home")
     else:
       print("Error while login")
   return render(request, "accounts/login.html", context)
