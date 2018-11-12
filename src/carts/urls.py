9684066dcfd44dc3eb38d5f03d4537572825d5aa
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-12 11:52:03 +0200

    checkout

--- src/carts/urls.py
+++ src/carts/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 
 from .views import (
   cart_home,
-  cart_update
+  cart_update,
+  checkout_home
   )
 
 urlpatterns = [
   url(r'^$', cart_home, name='home'),
   url(r'^update/$', cart_update, name='update'),
+  url(r'^checkout/$', checkout_home, name='checkout'),
 ]
 
