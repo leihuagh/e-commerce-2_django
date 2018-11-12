751014a4f8a3ebd772d91a11665e17d9358aba67
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-12 11:27:41 +0200

    calculate order total

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 
 from ecommerce.utils import unique_order_id_generator
 from carts.models import Cart
@@ -25,12 +25,38 @@ class Order(models.Model):
   def __str__(self):
     return self.order_id
   
+  def update_total(self):
+    cart_total = self.cart.total
+    shipping_total = self.shipping_total
+    new_total = cart_total + shipping_total
+    self.total = new_total
+    self.save()
+    return new_total
+  
 
 
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
   if not instance.order_id:
     instance.order_id = unique_order_id_generator(instance)
 
-
-
 pre_save.connect(pre_save_create_order_id, sender=Order)
+
+
+def post_save_cart_total(sender, instance, created, *args, **kwargs):
+  if not created:
+    cart_obj = instance
+    cart_total = cart_obj.total
+    cart_id = cart_obj.id
+    qs = Order.objects.filter(cart__id=cart_id)
+    if qs.count() == 1:
+      order_obj = qs.first()
+      order_obj.update_total()
+
+post_save.connect(post_save_cart_total, sender=Cart)
+
+
+def post_save_order(sender, instance, created, *args, **kwargs):
+  if created:
+    instance.update_total()
+
+post_save.connect(post_save_order, sender=Order)
