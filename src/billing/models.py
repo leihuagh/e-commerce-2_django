655386387da50ca11bc0faeff5b7b77ad6225297
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-13 21:46:03 +0200

    edit billing model signals

--- src/billing/models.py
+++ src/billing/models.py
@@ -20,7 +20,7 @@ class BillingProfile(models.Model):
 
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
-  if created:
-    BillingProfile.objects.get_or_create(user=instance)
+  if created and instance.email:
+    BillingProfile.objects.get_or_create(user=instance, email=instance.email)
 
 post_save.connect(user_created_receiver, sender=User)
