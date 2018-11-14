3ef594c11a36b15edd5e601ac8c984f7588a8116
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-14 10:25:38 +0200

    billing profile manager

--- src/billing/models.py
+++ src/billing/models.py
@@ -3,10 +3,27 @@ from django.db.models.signals import post_save
 
 from django.conf import settings
 
+from accounts.models import GuestEmail
+
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
 
+
+class BillingProfileManager(models.Manager):
+  def new_or_get(self, request):
+    user = request.user
+    guest_email_id = request.session.get('guest_email_id')
+    obj = None
+    created = False
+    if user.is_authenticated():
+      obj, created = self.model.objects.get_or_create(user=user, email=user.email)
+    elif guest_email_id is not None:
+      guset_email_obj = GuestEmail.objects.get(id=guest_email_id)
+      obj, created = self.model.objects.get_or_create(email=guset_email_obj.email)
+    else:
+      pass
+    return obj, created
 
 class BillingProfile(models.Model):
   user = models.OneToOneField(User, null=True, blank=True)
@@ -17,6 +34,8 @@ class BillingProfile(models.Model):
 
   def __str__(self):
     return self.email
+
+  objects = BillingProfileManager()
 
 
 def user_created_receiver(sender, instance, created, *args, **kwargs):
--- src/carts/views.py
+++ src/carts/views.py
@@ -39,19 +39,9 @@ def checkout_home(request):
   order_obj = None
   if cart_created or cart_obj.products.count() == 0:
     return redirect('cart:home')
-  user = request.user
-  billing_profile = None
   login_form = LoginForm()
   guest_form = GuestForm()
-  guest_email_id = request.session.get('guest_email_id')
-
-  if user.is_authenticated():
-    billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user=user, email=user.email)
-  elif guest_email_id is not None:
-    guset_email_obj = GuestEmail.objects.get(id=guest_email_id)
-    billing_profile, billing_guest_profile_created = BillingProfile.objects.get_or_create(email=guset_email_obj.email)
-  else:
-    pass
+  billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
   
   if billing_profile is not None:
     order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
