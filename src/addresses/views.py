431fb0cf0d4891704691a9aebaec2d49196cf7b9
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-14 12:57:10 +0200

    save address

--- src/addresses/views.py
+++ src/addresses/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 
 from .forms import AddressForm
+from billing.models import BillingProfile
 
 # Create your views here.
 
@@ -15,6 +16,14 @@ def checkout_address_create_view(request):
   redirect_path = next_ or next_post or None
   if form.is_valid():
     print(request.POST)
+    instance = form.save(commit=False)
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if billing_profile is not None:
+      instance.billing_profile = billing_profile
+      instance.address_type = request.POST.get('address_type', 'shipping')
+      instance.save()
+    else:
+      return redirect("cart:checkout")
     if is_safe_url(redirect_path, request.get_host()):
       return redirect(redirect_path)
     else:
