9066f03998af166505673ea71206b20e360631db
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-16 20:50:36 +0200

    auto search

--- src/static_local/js/auto_search.js
+++ src/static_local/js/auto_search.js
@@ -1,3 +1,29 @@
 $(document).ready(() => {
-  
+  const searchForm = $(".search-form");
+  const searchInput = searchForm.find("[name='q']");
+  let typingTimer;
+  const typingInterval = 500;
+  const searchBtn = searchForm.find("[type='submit']");
+
+  searchInput.keyup(event => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(performSearch, typingInterval);
+  });
+
+  searchInput.keydown(event => {
+    clearTimeout(typingTimer);
+  });
+
+  function displaySearching() {
+    searchBtn.addClass("disabled");
+    searchBtn.html("<i class='fa fa-spin fa-spinner'></i> Searching...");
+  }
+
+  function performSearch() {
+    displaySearching();
+    setTimeout(() => {
+      let query = searchInput.val();
+      window.location.href = `/search/?q=${query}`;
+    }, 1000)
+  }
 });
