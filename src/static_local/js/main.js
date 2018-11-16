d1567636fb623e66e089421cbfa7f9a97743601f
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-16 10:58:18 +0200

    refresh cart with ajax 1

--- src/static_local/js/main.js
+++ src/static_local/js/main.js
@@ -2,34 +2,59 @@ $(document).ready(() => {
   const productForm = $(".form-product-ajax");
   productForm.submit(function(e) {
     e.preventDefault();
-    // console.log(e.target.attributes.action.value);
-    // console.log(e.target.attributes.method.value);
-    // console.log(e.target.product_id.value);
-    
     const thisForm = $(this);
-    const actionEndpoint = thisForm.attr("action");
+    // const actionEndpoint = thisForm.attr("action");
+    const actionEndpoint = thisForm.attr("data-endpoint");
     const httpMethod = thisForm.attr("method");
-    const formData = thisForm.serialize()
-    console.log(productForm, thisForm, actionEndpoint, httpMethod, formData);
+    const formData = thisForm.serialize();
 
     $.ajax({
       url: actionEndpoint,
       method: httpMethod,
       data: formData,
       success: function(data) {
-        const submitSpan = thisForm.find('.submit-span')
+        const submitSpan = thisForm.find(".submit-span");
         if (data.added) {
-          submitSpan.html('In cart <button type="submit" class="btn btn-link">Remove</button>')
+          submitSpan.html(
+            'In cart <button type="submit" class="btn btn-link">Remove</button>'
+          );
         } else {
-          submitSpan.html('<button type="submit" class="btn btn-success">Add to cart</button>')
+          submitSpan.html(
+            '<button type="submit" class="btn btn-success">Add to cart</button>'
+          );
         }
-        const navbarCartCount = $('.navbar-cart-count')
-        navbarCartCount.text(data.cartItemCount)
+        const navbarCartCount = $(".navbar-cart-count");
+        navbarCartCount.text(data.cartItemCount);
+        const currentPath = window.location.href;
+        if (currentPath.indexOf("cart") != -1) {
+          refreshCart();
+        }
       },
       error: function(error) {
-        console.log('Error when add to cart => ', error)
+        console.log("Error when add to cart => ", error);
       }
-    })
-    console.log();
+    });
   });
+  console.log();
+
+  function refreshCart() {
+    const cartTable = $(".cart-table");
+    const cartBody = cartTable.find(".cart-body");
+    cartBody.html("<h1>changed</h1>");
+
+    const refreshCartUrl = "/api/cart/";
+    const refreshCartMethod = "GET";
+    const data = {};
+    $.ajax({
+      url: refreshCartUrl,
+      method: refreshCartMethod,
+      data: data,
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(error) {
+        console.log("Error when refresh cart => ", error);
+      }
+    });
+  }
 });
