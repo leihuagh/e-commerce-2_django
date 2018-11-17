aa70fa7afc083b29c0c975ef4c9203169758b03e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-17 09:52:01 +0200

    modify django admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
 
 from .models import GuestEmail
 
 # Register your models here.
 
-admin.site.register(GuestEmail)
+User = get_user_model()
+
+
+class UserAdmin(admin.ModelAdmin):
+  search_fields = ['email']
+  class Meta:
+    model = User
+
+
+
+admin.site.register(User, UserAdmin)
+
+
+
+class GuestEmailAdmin(admin.ModelAdmin):
+  search_fields = ['email']
+  class Meta:
+    model = GuestEmail
+
+
+admin.site.register(GuestEmail, GuestEmailAdmin)
