c3df34e61e84742facd93d6dd80653907f3f4ab4
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-18 12:12:25 +0200

    save analytics object viewed signal

--- src/analytics/models.py
+++ src/analytics/models.py
@@ -3,6 +3,9 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 
+from .signals import object_viewed_signal
+from .utils import get_client_ip
+
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
@@ -23,3 +26,16 @@ class ObjectViewed(models.Model):
     ordering = ['-timestamp']
     verbose_name = 'Object Viewed'
     verbose_name_plural = 'Objects Viewed'
+
+
+def object_viewed_receiver(sender, instance, request, *args, **kwargs):
+  c_type = ContentType.objects.get_for_model(sender) # == instance.__class__
+  new_view_obj = ObjectViewed.objects.create(
+    user=request.user,
+    object_id=instance.id,
+    content_type=c_type,
+    ip_address=get_client_ip(request),
+
+  )
+
+object_viewed_signal.connect(object_viewed_receiver)
--- src/analytics/utils.py
+++ src/analytics/utils.py
@@ -1,10 +1,9 @@
 
 
 def get_client_ip(request):
-  print(request.META)
   x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
   if x_forwarded_for:
     ip = x_forwarded_for.split(',')[0]
   else:
-    ip = request.META.get('REMOTE_ADDR')
+    ip = request.META.get('REMOTE_ADDR', None)
   return ip
