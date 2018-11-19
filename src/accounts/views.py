a107b2ae2e7b061f3eb299cfcd7290d369eb8678
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-19 09:35:04 +0200

    user session model

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse_lazy
 
 from .forms import LoginForm, RegisterForm, GuestForm
 from .models import GuestEmail
+from .signals import user_logged_in
 
 # Create your views here.
 
@@ -31,6 +32,7 @@ class LoginView(FormView):
     user = authenticate(request, username=email, password=password)
     if user is not None:
       login(request, user)
+      user_logged_in.send(user.__class__, instance=user, request=request)
       try:
         del request.session['guest_email_id']
       except:
--- src/analytics/models.py
+++ src/analytics/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
+from django.contrib.sessions.models import Session
+from django .db.models.signals import pre_save, post_save
 
+from accounts.signals import user_logged_in
 from .signals import object_viewed_signal
 from .utils import get_client_ip
 
@@ -39,3 +42,26 @@ def object_viewed_receiver(sender, instance, request, *args, **kwargs):
   )
 
 object_viewed_signal.connect(object_viewed_receiver)
+
+
+
+
+
+
+class UserSession(models.Model):
+  user            = models.ForeignKey(User, blank=True, null=True)
+  ip_address      = models.CharField(max_length=220, blank=True, null=True)
+  session_key     = models.CharField(max_length=100, blank=True, null=True)
+  timestamp       = models.DateTimeField(auto_now_add=True)
+  active          = models.BooleanField(default=True)
+  ended           = models.BooleanField(default=False)
+
+  def __str__(self, ):
+    return str(self.session_key)
+
+
+
+def user_logged_in_receiver(sender, instance, request, *args, **kwargs):
+  print(instance)
+
+user_logged_in.connect(user_logged_in_receiver)
