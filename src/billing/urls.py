af24e57cbf2f255eb4bbba4d3f19764ad625fae2
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-19 12:35:32 +0200

    improve payment method template

--- src/billing/urls.py
+++ src/billing/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 
 from .views import (
-  payment_method_view
+  payment_method_view,
+  payment_method_create_view,
+  billing_home
   )
 
 urlpatterns = [
+  url(r'^$', billing_home, name='home'),
   url(r'^payment-method/$', payment_method_view, name='payment_method'),
+  url(r'^payment-method/create/$', payment_method_create_view, name='create'),
 ]
 
--- src/billing/views.py
+++ src/billing/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.utils.http import is_safe_url
 
 # Create your views here.
 
@@ -7,9 +9,23 @@ stripe.api_key = 'sk_test_r3EsRHlzW559L1tojcPhYbBd'
 STRIPE_PUB_KEY = 'pk_test_7iZ5TStSI7YoApUV7UruHTB3'
 
 def payment_method_view(request):
-  if request.method == 'POST':
-    print(request.POST)
+  next_url = None
+  next_ = request.GET.get('next')
+  if is_safe_url(next_, request.get_host()):
+    next_url = next_
   context = {
-    'publish_key': STRIPE_PUB_KEY
+    'publish_key': STRIPE_PUB_KEY,
+    "next_url": next_url
   }
   return render(request, 'billing/payment-method.html', context)
+
+
+def payment_method_create_view(request):
+  if request.method == 'POST' and request.is_ajax():
+    print(request.POST)
+    return JsonResponse({"message": "Done"})
+  return HttpResponse("error", status_code=401)
+
+
+def billing_home(request):
+  return 'billing'
