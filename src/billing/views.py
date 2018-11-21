a29aece5cf7cd023d13b9d0db9ae7884bf227a72
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-21 11:17:02 +0200

    add card to customer in stripe

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
 from django.utils.http import is_safe_url
+
+from .models import BillingProfile
 
 # Create your views here.
 
@@ -9,6 +11,14 @@ stripe.api_key = 'sk_test_r3EsRHlzW559L1tojcPhYbBd'
 STRIPE_PUB_KEY = 'pk_test_7iZ5TStSI7YoApUV7UruHTB3'
 
 def payment_method_view(request):
+  # if request.user.is_authenticated():
+  #   billing_profile = request.user.billingprofile
+  #   my_customer_id = billing_profile.customer_id
+    
+  billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+  if not billing_profile:
+    return redirect('cart:home')
+
   next_url = None
   next_ = request.GET.get('next')
   if is_safe_url(next_, request.get_host()):
@@ -23,6 +33,15 @@ def payment_method_view(request):
 def payment_method_create_view(request):
   if request.method == 'POST' and request.is_ajax():
     print(request.POST)
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if not billing_profile:
+      return HttpResponse({"message": "can not find this user"}, status_code=401)
+    
+    token = request.POST.get('token')
+    if token is not None:
+      customer = stripe.Customer.retrieve(billing_profile.customer_id)
+      card_response = customer.sources.create(source=token)
+
     return JsonResponse({"message": "Success! Your cart was added successfully!"})
   return HttpResponse("error", status_code=401)
 
