e1e9aa97ab513ca38aca653dc8c1afad36a29e89
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-23 08:59:41 +0200

    loading spinner of payment form

--- src/static_local/js/stripe-billing.js
+++ src/static_local/js/stripe-billing.js
@@ -77,22 +77,71 @@ $(document).ready(function() {
       }
     });
 
+    // // Handle form submission.
+    // var form = document.getElementById("payment-form");
+    // form.addEventListener("submit", function(event) {
+    //   event.preventDefault();
+
+    //   stripe.createToken(card).then(function(result) {
+    //     if (result.error) {
+    //       // Inform the user if there was an error.
+    //       var errorElement = document.getElementById("card-errors");
+    //       errorElement.textContent = result.error.message;
+    //     } else {
+    //       // Send the token to your server.
+    //       stripeTokenHandler(nextUrl, result.token);
+    //     }
+    //   });
+    // });
+
+
     // Handle form submission.
-    var form = document.getElementById("payment-form");
-    form.addEventListener("submit", function(event) {
+    var form = $("#payment-form");
+    form.on("submit", function(event) {
       event.preventDefault();
+
+      var $this = $(this);
+      var btnLoad = $this.find('.btn-load');
+      btnLoad.blur();
+      var loadTime = 1500;
+      var currentTimeout;
+      var errorHtml = "<i class='fa fa-warning'></i> An error occurred";
+      var errorClasses = "btn btn-danger disabled my-3";
+      var loadingHtml = "<i class='fa fa-spin fa-spinner'></i> Loading...";
+      var loadingClasses = "btn btn-success disabled my-3";
 
       stripe.createToken(card).then(function(result) {
         if (result.error) {
           // Inform the user if there was an error.
-          var errorElement = document.getElementById("card-errors");
+          var errorElement = $("#card-errors");
           errorElement.textContent = result.error.message;
+          currentTimeout = displayBtnStatus(btnLoad, errorHtml, errorClasses, 1000, currentTimeout);
         } else {
           // Send the token to your server.
+          currentTimeout = displayBtnStatus(btnLoad, loadingHtml, loadingClasses, 2000, currentTimeout);
           stripeTokenHandler(nextUrl, result.token);
         }
       });
     });
+
+    function displayBtnStatus(element, newHtml, newClasses, loadTime, timeout){
+      // if (timeout){
+      //   clearTimeout(timeout);
+      // }
+      if (!loadTime){
+        loadTime = 1500;
+      }
+      var defaultHtml = element.html();
+      var defaultClasses = element.attr("class");
+      element.html(newHtml);
+      element.removeClass(defaultClasses);
+      element.addClass(newClasses);
+      return setTimeout(function(){
+          element.html(defaultHtml);
+          element.removeClass(newClasses);
+          element.addClass(defaultClasses);
+      }, loadTime);
+    }
 
     // Submit the form with the token ID.
     function stripeTokenHandler(nextUrl, token) {
