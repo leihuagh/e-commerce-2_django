6ac0332c3e87897c89cd0ff43e5f805dca842ddd
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-11-29 11:09:43 +0200

    prepare database config for deployment to heroku

--- src/ecommerce/settings/production.py
+++ src/ecommerce/settings/production.py
@@ -104,6 +104,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
