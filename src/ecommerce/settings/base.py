540e8afa20ab285ddae5d64c13da38ad6aab8d25
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2018-12-02 20:59:11 +0200

    set config file

--- src/ecommerce/settings/base.py
+++ src/ecommerce/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+from .config import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
