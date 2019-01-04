d4de0ef5c0e8a9e120b1df0dd1dd0caf738f95d5
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-04 11:34:59 +0200

    change stripe env key

--- src/billing/models.py
+++ src/billing/models.py
@@ -8,7 +8,8 @@ from accounts.models import GuestEmail
 
 # Create your models here.
 import stripe
-stripe.api_key = 'sk_test_r3EsRHlzW559L1tojcPhYbBd'
+STRIPE_SECRET_KEY = getattr(settings, 'STRIPE_SECRET_KEY', 'sk_test_r3EsRHlzW559L1tojcPhYbBd')
+stripe.api_key = STRIPE_SECRET_KEY
 
 User = settings.AUTH_USER_MODEL
 
