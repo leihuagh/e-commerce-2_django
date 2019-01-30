b59476bccf157af2b02023fbd8a044bc77318bb2
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-30 16:37:25 +0200

    customize billing profile admin

--- src/billing/admin.py
+++ src/billing/admin.py
@@ -4,6 +4,21 @@ from .models import BillingProfile, Card, Charge
 
 # Register your models here.
 
-admin.site.register(BillingProfile)
+
+class BillingProfileAdmin(admin.ModelAdmin):
+
+  list_display = ['id', '__str__', 'user', 'customer_id', 'active', 'timestamp', 'updated']
+  list_display_links = ['__str__']
+  list_filter = ['email', 'user', 'active']
+  search_fields = ['id', 'email', 'user__email', 'customer_id']
+  ordering = ['-timestamp']
+
+  class Meta:
+    model = BillingProfile
+
+
+admin.site.register(BillingProfile, BillingProfileAdmin)
+
+
 admin.site.register(Card)
 admin.site.register(Charge)
