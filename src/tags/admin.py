d5560e94254021494f4c9cd978a881ffedb3d017
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-30 17:54:53 +0200

    customize tags admin

--- src/tags/admin.py
+++ src/tags/admin.py
@@ -4,4 +4,17 @@ from django.contrib import admin
 
 from .models import Tag
 
-admin.site.register(Tag)
+
+class TagAdmin(admin.ModelAdmin):
+
+  list_display  = ['id', '__str__', 'slug', 'active', 'timestamp']
+  list_display_links = ['__str__']
+  list_filter = ['title', 'active']
+  search_fields = ['id', 'title']
+  ordering = ['-timestamp']
+  
+  class Meta:
+    model = Tag
+
+
+admin.site.register(Tag, TagAdmin)
