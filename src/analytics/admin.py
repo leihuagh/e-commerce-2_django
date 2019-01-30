8558ec09606006e7e89db727591f47659db4772b
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-30 17:02:56 +0200

    customize user sessions admin

--- src/analytics/admin.py
+++ src/analytics/admin.py
@@ -6,4 +6,18 @@ from .models import ObjectViewed, UserSession
 
 
 admin.site.register(ObjectViewed)
-admin.site.register(UserSession)
+
+
+class UserSessionAdmin(admin.ModelAdmin):
+
+  list_display = ['id', '__str__', 'ip_address', 'session_key', 'active', 'ended', 'timestamp']
+  list_display_links = ['__str__']
+  list_filter = ['active', 'ended']
+  search_fields = ['id', 'ip_address', 'session_key']
+  ordering = ['-timestamp']
+
+  class Meta:
+    model = UserSession
+
+
+admin.site.register(UserSession, UserSessionAdmin)
