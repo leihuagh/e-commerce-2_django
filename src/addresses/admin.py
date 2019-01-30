5171732dfbed6af60a7193e94533f6aeea4eaf73
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-30 17:25:40 +0200

    customize addresses admin

--- src/addresses/admin.py
+++ src/addresses/admin.py
@@ -4,4 +4,17 @@ from .models import Address
 
 # Register your models here.
 
-admin.site.register(Address)
+
+class AddressAdmin(admin.ModelAdmin):
+
+  list_display = ['id', '__str__', 'name', 'nickname', 'address_type', 'address_line_1', 'city', 'country', 'state', 'postal_code']
+  list_display_links = ['__str__']
+  list_filter = ['billing_profile__email', 'address_type', 'city', 'country', 'state']
+  search_fields = ['id', 'billing_profile__email', 'address_type', 'city', 'country', 'state']
+  ordering = ['-id']
+
+  class Meta:
+    model = Address
+
+
+admin.site.register(Address, AddressAdmin)
