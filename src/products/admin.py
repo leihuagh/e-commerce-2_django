9f5d1ff77d58aa73551e810cd6443f936b06735b
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-30 16:06:34 +0200

    customize products admin

--- src/products/admin.py
+++ src/products/admin.py
@@ -5,7 +5,14 @@ from django.contrib import admin
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-  list_display = ['__str__', 'slug', 'is_digital']
+
+  list_display = ['id', '__str__', 'slug', 'price', 'image', 'featured', 'active', 'is_digital', 'timestamp']
+  list_display_links = ['__str__']
+  list_editable = ['price']
+  list_filter = ['featured', 'active', 'is_digital']
+  search_fields = ['id', 'title', 'price']
+  ordering = ['-timestamp']
+
   class Meta:
     model = Product
 
