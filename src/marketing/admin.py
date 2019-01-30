d092a3f26928e421fcc7bc2c79095ed70f9609f1
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-30 16:14:24 +0200

    customize marketing admin

--- src/marketing/admin.py
+++ src/marketing/admin.py
@@ -8,6 +8,9 @@ class MarketingPreferenceAdmin(admin.ModelAdmin):
 
   list_display  = ['__str__', 'subscribed', 'mailchimp_subscribed', 'timestamp', 'updated']
   readonly_fields = ['mailchimp_msg', 'mailchimp_subscribed', 'timestamp', 'updated']
+  list_filter = ['subscribed', 'mailchimp_subscribed']
+  search_fields = ['user__email']
+  ordering = ['-timestamp']
   
   class Meta:
     model = MarketingPreference
