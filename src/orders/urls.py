1da97cf580c22f59a4453d45b4561623a7ee521a
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-30 18:29:29 +0200

    library cbv

--- src/orders/urls.py
+++ src/orders/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 
 from .views import (
   OrderListView,
-  OrderDetailView
+  OrderDetailView,
+  LibraryView
   )
 
 urlpatterns = [
   url(r'^$', OrderListView.as_view(), name='list'),
+  url(r'^library/$', LibraryView.as_view(), name='library'),
   url(r'^(?P<order_id>[0-9A-Za-z]+)/$', OrderDetailView.as_view(), name='detail'),
 ]
 
