2f61203d082a3e7d6325beb804b41a917bfdd32c
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-30 16:27:23 +0200

    customize cart admin

--- src/carts/admin.py
+++ src/carts/admin.py
@@ -4,4 +4,16 @@ from .models import Cart
 
 # Register your models here.
 
-admin.site.register(Cart)
+
+class CartAdmin(admin.ModelAdmin):
+
+  list_display = ['__str__', 'user', 'subtotal', 'total', 'timestamp']
+  list_display_links = ['__str__']
+  list_filter = ['user__email']
+  search_fields = ['id', 'user__email']
+  ordering = ['-timestamp']
+
+  class Meta:
+    model = Cart
+
+admin.site.register(Cart, CartAdmin)
