16c30a9077b73ed20552fb48ac40d2a682fc027e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-31 09:25:52 +0200

    add protected location in base, production settings

--- src/ecommerce/settings/production.py
+++ src/ecommerce/settings/production.py
@@ -179,6 +179,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 
+PROTECTED_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "protected_media")
 
 
 CORS_REPLACE_HTTPS_REFERER      = True
