258e372e30e36d5f5c6590f1f94f1f22f3c9e399
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-01-31 18:09:07 +0200

    download file permission

--- src/products/models.py
+++ src/products/models.py
@@ -118,6 +118,8 @@ class ProductFile(models.Model):
           upload_to=upload_product_file_loc, 
           storage=FileSystemStorage(location=settings.PROTECTED_ROOT)
   )
+  free = models.BooleanField(default=False)
+  user_required = models.BooleanField(default=False)
 
   def __str__(self):
     return str(self.file.name)
--- src/products/views.py
+++ src/products/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, View
 from django.http import Http404, HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
 from .models import Product, ProductFile
 from carts.models import Cart
+from orders.models import ProductPurchase
 from analytics.mixins import ObjectViewedMixin
 
 
@@ -96,13 +98,31 @@ class UserProductHistoryView(LoginRequiredMixin, ListView):
 
 
 class ProductDownloadView(View):
-  def get(self, *args, **kwargs):
+  def get(self, request, *args, **kwargs):
     slug = kwargs.get('slug')
     pk = kwargs.get('pk')
     downloads_qs = ProductFile.objects.filter(pk=pk, product__slug=slug)
     if downloads_qs.count() != 1:
       raise Http404("Download not found")
     download_obj = downloads_qs.first()
+
+    can_download = False
+    user_ready = True
+    if download_obj.user_required:
+      if not request.user.is_authenticated():
+        user_ready = False
+    purchased_products = Product.objects.none()
+    if download_obj.free:
+      can_download = True
+      user_ready = True
+    else:
+      purchased_products = ProductPurchase.objects.products_by_request(request)
+      if download_obj.product in purchased_products:
+        can_download = True
+    if not can_download or not user_ready:
+      messages.error(request, "You do not have access to download this item")
+      return redirect(download_obj.get_default_url())
+
     file_root = settings.PROTECTED_ROOT
     filepath = download_obj.file.path
     final_filepath = os.path.join(file_root, filepath)
