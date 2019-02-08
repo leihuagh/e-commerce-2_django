201bd8440a361ba0bfbe556d2cbdedd51ecb8a7e
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-02-08 15:53:04 +0200

    make new migrations

--- src/carts/models.py
+++ src/carts/models.py
@@ -36,8 +36,8 @@ class CartManager(models.Manager):
 class Cart(models.Model):
   user = models.ForeignKey(User, null=True, blank=True)
   products = models.ManyToManyField(Product, blank=True)
-  subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
-  total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+  subtotal = models.DecimalField(default=0.00, max_digits=60, decimal_places=2)
+  total = models.DecimalField(default=0.00, max_digits=60, decimal_places=2)
   timestamp = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
 
