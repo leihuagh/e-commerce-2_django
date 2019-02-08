ac8c71d88327daeea22c8647a28bf9f97c40f47a
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-02-08 15:46:50 +0200

    clean old migrations

--- src/ecommerce/settings/production.py
+++ src/ecommerce/settings/production.py
@@ -122,8 +122,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'e_commerce',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'OSAMA',
+        'PASSWORD': 'OSAMA',
     }
 }
 
