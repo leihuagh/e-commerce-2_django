6ec1294622f92f4747b209d4723baaf66e91d35a
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-02-11 14:08:52 +0200

    change checlout address create from fbv to cbv

--- src/addresses/urls.py
+++ src/addresses/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from .views import (
-  checkout_address_create_view,
+  CheckoutAddressCreateView,
   CheckoutAddressReuseView,
   AddressListView,
   AddressCreateView,
@@ -9,7 +9,7 @@ from .views import (
   )
 
 urlpatterns = [
-  url(r'^checkout/address/create/$', checkout_address_create_view, name='create'),
+  url(r'^checkout/address/create/$', CheckoutAddressCreateView.as_view(), name='create'),
   url(r'^checkout/address/reuse/$', CheckoutAddressReuseView.as_view(), name='reuse'),
   url(r'^$', AddressListView.as_view(), name='list'),
   url(r'^create/$', AddressCreateView.as_view(), name='address-create'),
--- src/addresses/views.py
+++ src/addresses/views.py
@@ -46,31 +46,53 @@ class AddressUpdateView(LoginRequiredMixin, UpdateView):
     return Address.objects.filter(billing_profile=billing_profile)
 
 
+class CheckoutAddressCreateView(View):
+
+  def post(self, request):
+    form = AddressCheckoutForm(request.POST or None)
+    next_ = request.GET.get('next')
+    next_post = request.POST.get('next')
+    redirect_path = next_ or next_post or None
+    if form.is_valid():
+      instance = form.save(commit=False)
+      billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+      if billing_profile is not None:
+        address_type = request.POST.get('address_type', 'shipping')
+        instance.billing_profile = billing_profile
+        instance.address_type = address_type
+        instance.save()
+
+        request.session[address_type + '_address_id'] = instance.id
+      else:
+        return redirect("cart:checkout")
+      if is_safe_url(redirect_path, request.get_host()):
+        return redirect(redirect_path)
+    return redirect("cart:checkout")
 
 
-def checkout_address_create_view(request):
-  form = AddressCheckoutForm(request.POST or None)
-  context = {
-    "form": form
-  }
-  next_ = request.GET.get('next')
-  next_post = request.POST.get('next')
-  redirect_path = next_ or next_post or None
-  if form.is_valid():
-    instance = form.save(commit=False)
-    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
-    if billing_profile is not None:
-      address_type = request.POST.get('address_type', 'shipping')
-      instance.billing_profile = billing_profile
-      instance.address_type = address_type
-      instance.save()
+# def checkout_address_create_view(request):
+#   form = AddressCheckoutForm(request.POST or None)
+#   context = {
+#     "form": form
+#   }
+#   next_ = request.GET.get('next')
+#   next_post = request.POST.get('next')
+#   redirect_path = next_ or next_post or None
+#   if form.is_valid():
+#     instance = form.save(commit=False)
+#     billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+#     if billing_profile is not None:
+#       address_type = request.POST.get('address_type', 'shipping')
+#       instance.billing_profile = billing_profile
+#       instance.address_type = address_type
+#       instance.save()
 
-      request.session[address_type + '_address_id'] = instance.id
-    else:
-      return redirect("cart:checkout")
-    if is_safe_url(redirect_path, request.get_host()):
-      return redirect(redirect_path)
-  return redirect("cart:checkout")
+#       request.session[address_type + '_address_id'] = instance.id
+#     else:
+#       return redirect("cart:checkout")
+#     if is_safe_url(redirect_path, request.get_host()):
+#       return redirect(redirect_path)
+#   return redirect("cart:checkout")
 
 
 class CheckoutAddressReuseView(View):
