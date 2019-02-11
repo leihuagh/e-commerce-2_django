97b036b05e5db68b27f196b10c42fc10d5f95e21
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-02-11 14:03:34 +0200

    change checlout address reuse from fbv to cbv

--- src/addresses/urls.py
+++ src/addresses/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 
 from .views import (
   checkout_address_create_view,
-  checkout_address_reuse_view,
+  CheckoutAddressReuseView,
   AddressListView,
   AddressCreateView,
   AddressUpdateView,
@@ -10,7 +10,7 @@ from .views import (
 
 urlpatterns = [
   url(r'^checkout/address/create/$', checkout_address_create_view, name='create'),
-  url(r'^checkout/address/reuse/$', checkout_address_reuse_view, name='reuse'),
+  url(r'^checkout/address/reuse/$', CheckoutAddressReuseView.as_view(), name='reuse'),
   url(r'^$', AddressListView.as_view(), name='list'),
   url(r'^create/$', AddressCreateView.as_view(), name='address-create'),
   url(r'^update/(?P<pk>\d+)/$', AddressUpdateView.as_view(), name='address-update'),
--- src/addresses/views.py
+++ src/addresses/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.views.generic import View
 
 from .forms import AddressCheckoutForm, AddressForm
 from billing.models import BillingProfile
@@ -72,21 +73,41 @@ def checkout_address_create_view(request):
   return redirect("cart:checkout")
 
 
-def checkout_address_reuse_view(request):
-  if request.user.is_authenticated():
-    context = {}
-    next_ = request.GET.get('next')
-    next_post = request.POST.get('next')
-    redirect_path = next_ or next_post or None
+class CheckoutAddressReuseView(View):
 
-    if request.method == 'POST':
-      shipping_address = request.POST.get('shipping_address', None)
-      address_type = request.POST.get('address_type', 'shipping')
-      billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
-      if shipping_address is not None:
-        qs = Address.objects.filter(billing_profile=billing_profile, id=shipping_address)
-        if qs.exists():
-          request.session[address_type + '_address_id'] = shipping_address
-        if is_safe_url(redirect_path, request.get_host()):
-          return redirect(redirect_path)
-  return redirect("cart:checkout")
+  def post(self, request):
+    if request.user.is_authenticated():
+      next_ = request.GET.get('next')
+      next_post = request.POST.get('next')
+      redirect_path = next_ or next_post or None
+
+      if request.method == 'POST':
+        shipping_address = request.POST.get('shipping_address', None)
+        address_type = request.POST.get('address_type', 'shipping')
+        billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+        if shipping_address is not None:
+          qs = Address.objects.filter(billing_profile=billing_profile, id=shipping_address)
+          if qs.exists():
+            request.session[address_type + '_address_id'] = shipping_address
+          if is_safe_url(redirect_path, request.get_host()):
+            return redirect(redirect_path)
+    return redirect("cart:checkout")
+
+
+# def checkout_address_reuse_view(request):
+#   if request.user.is_authenticated():
+#     next_ = request.GET.get('next')
+#     next_post = request.POST.get('next')
+#     redirect_path = next_ or next_post or None
+
+#     if request.method == 'POST':
+#       shipping_address = request.POST.get('shipping_address', None)
+#       address_type = request.POST.get('address_type', 'shipping')
+#       billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+#       if shipping_address is not None:
+#         qs = Address.objects.filter(billing_profile=billing_profile, id=shipping_address)
+#         if qs.exists():
+#           request.session[address_type + '_address_id'] = shipping_address
+#         if is_safe_url(redirect_path, request.get_host()):
+#           return redirect(redirect_path)
+#   return redirect("cart:checkout")
