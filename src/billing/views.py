64da36df57ba2730ab2e9ddcc4cef0ae518c9dfc
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-02-11 13:08:00 +0200

    change payment method from fbv to cbv

--- src/billing/views.py
+++ src/billing/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
 from django.utils.http import is_safe_url
 from django.conf import settings
+from django.views.generic import ListView, TemplateView, View
 
 from .models import BillingProfile, Card
 
@@ -13,24 +14,43 @@ STRIPE_SECRET_KEY = getattr(settings, 'STRIPE_SECRET_KEY')
 STRIPE_PUB_KEY = getattr(settings, 'STRIPE_PUB_KEY')
 stripe.api_key = STRIPE_SECRET_KEY
 
-def payment_method_view(request):
-  # if request.user.is_authenticated():
-  #   billing_profile = request.user.billingprofile
-  #   my_customer_id = billing_profile.customer_id
-    
-  billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
-  if not billing_profile:
-    return redirect('cart:home')
 
-  next_url = None
-  next_ = request.GET.get('next')
-  if is_safe_url(next_, request.get_host()):
-    next_url = next_
-  context = {
-    'publish_key': STRIPE_PUB_KEY,
-    "next_url": next_url
-  }
-  return render(request, 'billing/payment-method.html', context)
+
+class PaymentMethodView(View):
+  template_name = 'billing/payment-method.html'
+
+  def get(self, request):
+    billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+    if not billing_profile:
+      return redirect('cart:home')
+    next_url = None
+    next_ = request.GET.get('next')
+    if is_safe_url(next_, request.get_host()):
+      next_url = next_
+    context = {
+      'publish_key': STRIPE_PUB_KEY,
+      "next_url": next_url
+    }
+    return render(request, self.template_name, context)
+
+# def payment_method_view(request):
+#   # if request.user.is_authenticated():
+#   #   billing_profile = request.user.billingprofile
+#   #   my_customer_id = billing_profile.customer_id
+    
+#   billing_profile, billing_profile_created = BillingProfile.objects.new_or_get(request)
+#   if not billing_profile:
+#     return redirect('cart:home')
+
+#   next_url = None
+#   next_ = request.GET.get('next')
+#   if is_safe_url(next_, request.get_host()):
+#     next_url = next_
+#   context = {
+#     'publish_key': STRIPE_PUB_KEY,
+#     "next_url": next_url
+#   }
+#   return render(request, 'billing/payment-method.html', context)
 
 
 def payment_method_create_view(request):
@@ -46,7 +66,7 @@ def payment_method_create_view(request):
       # new_card_obj = Card.objects.add_new(billing_profile, card_response)
       new_card_obj = Card.objects.add_new(billing_profile, token)
     return JsonResponse({"message": "Success! Your cart was added successfully!"})
-  return HttpResponse("error", status_code=401)
+  return HttpResponse("error", status=401)
 
 
 def billing_home(request):
