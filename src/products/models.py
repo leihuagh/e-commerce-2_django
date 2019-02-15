a206f20713077213db9c7fbec1148ef59b25ae41
OSAMA MOHAMED <om1234@fayoum.edu.eg>
2019-02-15 17:03:36 +0200

    redesign cart buttons

--- src/products/models.py
+++ src/products/models.py
@@ -76,6 +76,9 @@ class Product(models.Model):
 
   objects = ProductManager()
 
+  class Meta:
+    ordering = ['-id']
+
   def get_absolute_url(self):
     return reverse('products:detail', kwargs={'slug': self.slug})
   
